85be37c07626524ace82bf599ce501aad8156489
Norrrrrrue <a07911116@qq.com>
2023-08-06 11:24:18 +0800

    Add files via upload

---
+++ source/PointSwitch/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace MyPointSwitchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(322, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(528, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(322, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(48, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(322, 21);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(528, 135);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(322, 21);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "替换目标";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(892, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "替换目标";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(411, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "加载目录";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(892, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "加载目录";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(402, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "重新加载";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(402, 428);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "一键替换";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(892, 292);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "重新加载";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(892, 428);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "一键替换";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // treeView1
+            // 
+            this.treeView1.AllowDrop = true;
+            this.treeView1.Location = new System.Drawing.Point(48, 192);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(322, 367);
+            this.treeView1.TabIndex = 12;
+            // 
+            // treeView2
+            // 
+            this.treeView2.AllowDrop = true;
+            this.treeView2.Location = new System.Drawing.Point(528, 192);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(322, 367);
+            this.treeView2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PointSwitch.Properties.Resources._0b7b02087bf40ad1021630eb552c11dfa8eccecc;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1082, 606);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "点位切换器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+    }
+}
+
---
+++ source/PointSwitch/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPointSwitchApp
+{
+    public partial class Form1 : Form
+    {
+        private string iniFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 获取程序根目录下的INI文件路径
+            iniFilePath = Path.Combine(Application.StartupPath, "config.ini");
+
+            // 在窗体加载时从INI文件中加载配置信息
+            LoadConfigFromIni();
+
+        }
+
+        private void LoadConfigFromIni()
+        {
+            try
+            {
+                // 检查INI文件是否存在
+                if (File.Exists(iniFilePath))
+                {
+                    // 创建一个INI文件读取器
+                    using (StreamReader reader = new StreamReader(iniFilePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // 解析INI文件中的配置项
+                            if (line.StartsWith("TextBox1Value="))
+                            {
+                                textBox1.Text = line.Substring("TextBox1Value=".Length);
+                            }
+                            else if (line.StartsWith("TextBox2Value="))
+                            {
+                                textBox2.Text = line.Substring("TextBox2Value=".Length);
+                            }
+                            else if (line.StartsWith("TextBox3Value="))
+                            {
+                                textBox3.Text = line.Substring("TextBox3Value=".Length);
+                            }
+                            else if (line.StartsWith("TextBox4Value="))
+                            {
+                                textBox4.Text = line.Substring("TextBox4Value=".Length);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"从INI文件加载配置信息时发生错误：{ex.Message}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowser.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox1.Text = folderBrowser.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowser.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox2.Text = folderBrowser.SelectedPath;
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowser.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox3.Text = folderBrowser.SelectedPath;
+                    ShowSubDirectoriesInTreeView(textBox3.Text, treeView1);
+                }
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            using (var folderBrowser = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowser.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    textBox4.Text = folderBrowser.SelectedPath;
+                    ShowSubDirectoriesInTreeView(textBox4.Text, treeView2);
+                }
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            ShowSubDirectoriesInTreeView(textBox3.Text, treeView1);
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            ShowSubDirectoriesInTreeView(textBox4.Text, treeView2);
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            string sourceFolderPath = textBox1.Text;
+            TreeNode selectedNode = treeView1.SelectedNode;
+
+            if (selectedNode == null)
+            {
+                MessageBox.Show("请先选择一个文件夹。");
+                return;
+            }
+
+            string targetFolderPath = Path.Combine(textBox3.Text, selectedNode.FullPath);
+            ClearFolder(sourceFolderPath);
+            int replacedFilesCount = CopyFiles(targetFolderPath, sourceFolderPath);
+            MessageBox.Show($"已成功替换 {replacedFilesCount} 个文件。");
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            string sourceFolderPath = textBox2.Text;
+            TreeNode selectedNode = treeView2.SelectedNode;
+
+            if (selectedNode == null)
+            {
+                MessageBox.Show("请先选择一个文件夹。");
+                return;
+            }
+
+            string targetFolderPath = Path.Combine(textBox4.Text, selectedNode.FullPath);
+            ClearFolder(sourceFolderPath);
+            int replacedFilesCount = CopyFiles(targetFolderPath, sourceFolderPath);
+            MessageBox.Show($"已成功替换 {replacedFilesCount} 个文件。");
+        }
+
+        private void ShowSubDirectoriesInTreeView(string folderPath, System.Windows.Forms.TreeView treeView)
+        {
+            treeView.Nodes.Clear();
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(folderPath);
+                foreach (string subDirectory in subDirectories)
+                {
+                    string folderName = new DirectoryInfo(subDirectory).Name;
+                    TreeNode node = new TreeNode(folderName);
+                    treeView.Nodes.Add(node);
+                    ShowSubDirectoriesRecursive(subDirectory, node);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("没有权限访问该文件夹。");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("文件夹不存在。");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("发生IO错误。");
+            }
+        }
+
+        private void ShowSubDirectoriesRecursive(string path, TreeNode parentNode)
+        {
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(path);
+                foreach (string subDirectory in subDirectories)
+                {
+                    string folderName = new DirectoryInfo(subDirectory).Name;
+                    TreeNode node = new TreeNode(folderName);
+                    parentNode.Nodes.Add(node);
+                    ShowSubDirectoriesRecursive(subDirectory, node);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Handle unauthorized access exception, skip this folder
+            }
+            catch (DirectoryNotFoundException)
+            {
+                // Handle directory not found exception, skip this folder
+            }
+            catch (IOException)
+            {
+                // Handle other IO errors, skip this folder
+            }
+        }
+
+        private void ClearFolder(string folderPath)
+        {
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(folderPath);
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+                {
+                    subDirectory.Delete(true);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("没有权限访问文件夹。");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("文件夹不存在。");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("发生IO错误。");
+            }
+        }
+
+        private int CopyFiles(string sourceFolderPath, string targetFolderPath)
+        {
+            int replacedFilesCount = 0;
+            try
+            {
+                Directory.CreateDirectory(targetFolderPath);
+                foreach (string sourceFilePath in Directory.GetFiles(sourceFolderPath))
+                {
+                    string fileName = Path.GetFileName(sourceFilePath);
+                    string targetFilePath = Path.Combine(targetFolderPath, fileName);
+                    File.Copy(sourceFilePath, targetFilePath, true);
+                    replacedFilesCount++;
+                }
+                foreach (string sourceSubFolderPath in Directory.GetDirectories(sourceFolderPath))
+                {
+                    string subFolderName = new DirectoryInfo(sourceSubFolderPath).Name;
+                    string targetSubFolderPath = Path.Combine(targetFolderPath, subFolderName);
+                    replacedFilesCount += CopyFiles(sourceSubFolderPath, targetSubFolderPath);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("没有权限访问文件夹。");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("文件夹不存在。");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("发生IO错误。");
+            }
+
+            return replacedFilesCount;
+        }
+
+        private void SaveConfigToIni()
+        {
+            try
+            {
+                // 创建一个INI文件写入器
+                using (StreamWriter writer = new StreamWriter(iniFilePath))
+                {
+                    // 写入textBox1、textBox2、textBox3和textBox4的内容到INI文件
+                    writer.WriteLine("[Config]");
+                    writer.WriteLine($"TextBox1Value={textBox1.Text}");
+                    writer.WriteLine($"TextBox2Value={textBox2.Text}");
+                    writer.WriteLine($"TextBox3Value={textBox3.Text}");
+                    writer.WriteLine($"TextBox4Value={textBox4.Text}");
+
+                    // 确保数据被立即写入到文件中
+                    writer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                // 错误处理代码，您可以选择输出错误日志或记录日志文件等，而不是弹出消息框
+                Console.WriteLine($"从INI文件加载配置信息时发生错误：{ex.Message}");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // 当 textBox1 文本内容发生变化时，保存配置信息到INI文件
+            SaveConfigToIni();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // 当 textBox2 文本内容发生变化时，保存配置信息到INI文件
+            SaveConfigToIni();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // 当 textBox3 文本内容发生变化时，保存配置信息到INI文件
+            SaveConfigToIni();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            // 当 textBox4 文本内容发生变化时，保存配置信息到INI文件
+            SaveConfigToIni();
+        }
+    }
+}
